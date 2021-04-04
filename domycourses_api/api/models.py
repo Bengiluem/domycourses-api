03ad686898e442d9bf0748d4051971987a59131c
LuluDavid <davidlulu92@yahoo.fr>
2021-04-04 23:26:26 +0200

    Adding db models

--- domycourses_api/api/models.py
+++ domycourses_api/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    ingredients = models.ManyToManyField(Ingredient)
