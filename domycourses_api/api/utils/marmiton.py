0b549fde787e4749c32034f04336d546f749a158
Emile Tireau <e.tireau@dental-monitoring.com>
2021-04-05 00:14:51 +0200

    add combination of marmiton results

--- domycourses_api/api/utils/marmiton.py
+++ domycourses_api/api/utils/marmiton.py
@@ -6,6 +6,10 @@ class MarmitonScrapperException(Exception):
     pass
 
 
+class MarmitonCombinerException(Exception):
+    pass
+
+
 class MarmitonScrapper(object):
 
     @staticmethod
@@ -25,10 +29,43 @@ class MarmitonScrapper(object):
                 name = MarmitonScrapper.__clean_text(ingredient.find(class_='ingredient-name'))
                 complement = MarmitonScrapper.__clean_text(ingredient.find(class_='ingredient-complement'))
                 parsed_ingredients[name] = {
-                    "quantity": quantity if quantity != "" else None,
+                    "quantity": float(quantity) if quantity != "" else None,
                     "unit": unit if unit != "" else None,
                     "complement": complement if complement != "" else None
                 }
         except Exception:
             raise MarmitonScrapperException
         return parsed_ingredients
+
+
+class MarmitonUnitCombiner(object):
+
+    grammes = ['kg', 'g']
+    litres = ['l', 'ml']
+
+    @staticmethod
+    def combine(value1, value2):
+        if value1["unit"] == value2["unit"]:
+            return {
+                "unit": value1["unit"],
+                "quantity": value1["quantity"] + value2["quantity"]
+            }
+        else:
+            if value1["unit"] in MarmitonUnitCombiner.grammes and value2["unit"] in MarmitonUnitCombiner.grammes:
+                units = MarmitonUnitCombiner.grammes
+            elif value1["unit"] in MarmitonUnitCombiner.litres and value2["unit"] in MarmitonUnitCombiner.litres:
+                units = MarmitonUnitCombiner.litres
+            else:
+                raise MarmitonCombinerException
+            index1 = units.index(value1["unit"])
+            index2 = units.index(value2["unit"])
+            min_index = min(index1, index2)
+            return {
+                "unit": units[min_index],
+                "quantity": (value1 / (1000 ^ (index1 - min_index))) + (value2 / (1000 ^ (index2 - min_index)))
+            }
+
+
+
+
+
--- domycourses_api/api/utils/scrapper.py
+++ domycourses_api/api/utils/scrapper.py
@@ -1,9 +1,31 @@
 import logging
-from .marmiton import MarmitonScrapper
+from .marmiton import MarmitonScrapper, MarmitonScrapperException, MarmitonUnitCombiner, MarmitonCombinerException
 
 
 def get_items(urls):
+    recipes = []
     for url in urls:
         if "https://www.marmiton.org/" in url:
             logging.info(f"Getting {url}")
-            MarmitonScrapper.parse_url(url)
+            try:
+                recipes.append(MarmitonScrapper.parse_url(url))
+            except MarmitonScrapperException:
+                logging.error(f"Impossible to get {url}")
+    return assemble(recipes)
+
+
+def assemble(recipes):
+    ingredients = {}
+    for recipe in recipes:
+        for ingredient_name, ingredient_details in recipe:
+            if ingredient_name in ingredients:
+                try:
+                    combination = MarmitonUnitCombiner.combine(ingredient_details, ingredients[ingredient_name])
+                    ingredients[ingredient_name]["unit"] = combination["unit"]
+                    ingredients[ingredient_name]["quantity"] = combination["quantity"]
+                    ingredients[ingredient_name]["complement"] += f", {ingredient_details['complement']}"
+                except MarmitonCombinerException:
+                    logging.error(f"Impossible to combine {ingredient_details['quantity']} {ingredient_details['unit']} with {ingredients[ingredient_name]['quantity']} {ingredients[ingredient_name]['unit']}")
+            else:
+                ingredients[ingredient_name] = ingredient_details
+    return ingredients
